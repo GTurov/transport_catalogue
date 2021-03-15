76b3f5f5dee44a686186c69fd27f5333c5ee769d
Gennadiy Turov <mirage216@mail.ru>
2021-03-15 23:12:02 +0400

    evening1

--- transport_router.cpp
+++ transport_router.cpp
@@ -1,2 +1,58 @@
 #include "transport_router.h"
 
+namespace transport {
+
+
+DistanceFinder::DistanceFinder(Catalogue& catalogue, const Route* route)
+    : direct_distances_(route->stops().size(),0),
+      reverse_distances_(route->stops().size(),0)
+{
+    int directDistanceSum = 0;
+    int reverseDistanceSum = 0;
+    for (int i = 1; i < (int)route->stops().size(); ++i) {
+        directDistanceSum += catalogue.distanceBetween(route->stops()[i-1],route->stops()[i]);
+        direct_distances_[i] = directDistanceSum;
+        reverseDistanceSum += catalogue.distanceBetween(route->stops()[i],route->stops()[i-1]);
+        reverse_distances_[i] = reverseDistanceSum;
+        //std::cerr << i << " " << direct_distances_[i] << " " << reverse_distances_[i]<<std::endl;
+    }
+}
+
+int DistanceFinder::distanceBetween(int fromStopIndex, int toStopIndex) {
+    if (fromStopIndex < toStopIndex) {
+        return direct_distances_[toStopIndex] - direct_distances_[fromStopIndex];
+    } else {
+        return -(reverse_distances_[toStopIndex] - reverse_distances_[fromStopIndex]);
+    }
+}
+
+RouteFinder::RouteFinder(Catalogue& catalogue, int bus_wait_time , double bus_velocity)
+    : catalogue_(catalogue),
+      bus_wait_time_(bus_wait_time),
+      bus_velocity_(bus_velocity/3.6) {
+    // Все остановки будут вершинами графа. Добавим их в словарь для быстрого поиска вершины по названию.
+    for (auto* stop: catalogue_.allStops()) {
+        graphVertexes_.push_back(stop);
+        stopToGraphVertex_.insert({stop->name(),graphVertexes_.size()-1});
+    }
+
+    for (auto* route: catalogue_.allRoutes()) {
+        DistanceFinder df(catalogue,route);
+        const auto& stops = route->stops();
+        if (route->isCycled()) {
+
+        } else {
+            for (int i = 0; i < (int)stops.size(); ++i) {
+                for (int j = 0; j < (int)stops.size(); ++j) {
+                    if (i != j) {
+                        TripItem item{stops[i], stops[j], abs(i-j), df.distanceBetween(i,j)/bus_velocity_};
+                        std::cerr<<"Eblo "<<item << ", "s<<df.distanceBetween(i,j)<<" m"<<std::endl;
+                    }
+                }
+            }
+        }
+    }
+
+}
+
+} // namespace transport
--- transport_router.h
+++ transport_router.h
@@ -10,41 +10,53 @@
 
 namespace transport {
 
-struct Trip {
+struct TripTime {
+    double wait = 0;
+    double ride = 0;
+};
+
+struct TripItem {
     transport::Stop* from = nullptr;
     transport::Stop* to = nullptr;
     int stopCount = 0;
     double time = 0;
 };
 
+inline std::ostream& operator<<(std::ostream& out, const TripItem& item) {
+    out << item.from->name() << " -> "s << item.to->name()
+        << ": " << item.stopCount << " stops, " << item.time << " sec trip time";
+    return out;
+}
+
+// Вспомогательный класс для вычисления расстояния между всеми остановками
+// за линейное время
+class DistanceFinder {
+public:
+    DistanceFinder(Catalogue& catalogue, const Route* route);
+    int distanceBetween(int fromStopIndex, int toStopIndex);
+private:
+    std::vector<int> direct_distances_;
+    std::vector<int> reverse_distances_;
+
+};
+
 class RouteFinder {
     using route = graph::Router<double>::RouteInfo;
+
 public:
-    RouteFinder(Catalogue& catalogue, int bus_wait_time , double bus_velocity)
-        : catalogue_(catalogue),
-          bus_wait_time_(bus_wait_time),
-          bus_velocity_(bus_velocity) {
-        // Все остановки будут вершинами графа. Добавим их в словарь для быстрого поиска вершины по названию.
-        for (auto* stop: catalogue_.allStops()) {
-            graphVertexes_.push_back(stop);
-            stopToGraphVertex_.insert({stop->name(),graphVertexes_.size()-1});
-        }
-        for (auto* route: catalogue_.allRoutes()) {
+    RouteFinder(Catalogue& catalogue, int bus_wait_time , double bus_velocity);
+    std::optional<std::vector<TripItem>> findRoute(std::string_view from, std::string_view to);
 
-        }
-
-    }
-    std::optional<route> getRoute(std::string_view from, std::string_view to) {
-        return std::nullopt;
-    }
+private:
+    std::optional<route> buildRoute(std::string_view from, std::string_view to);
 
 private:
     Catalogue& catalogue_;
     //graph::Router<double> router_;
     graph::DirectedWeightedGraph<double> graph_;
-    std::unordered_map<std::string_view, int> stopToGraphVertex_;
     std::vector<transport::Stop*> graphVertexes_;
-    std::vector<Trip> graphEdges_;
+    std::unordered_map<std::string_view, int> stopToGraphVertex_;
+    std::vector<TripItem> graphEdges_;
     int bus_wait_time_ = 0;
     double bus_velocity_ = 0;
 
